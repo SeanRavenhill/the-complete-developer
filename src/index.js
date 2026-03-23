8cac2c131881963d9daa5fd64b6433082e6af0a4
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2026-03-23 17:42:58 +0000

    updated package.json to ES.Next Modules. Updated index.js to use modern JS with module imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+import express from 'express';
 const server = express();
 const port = 3000;
 
 server.get('/hello', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World with Modules!');
 });
 
 server.listen(port, function () {
