0ce963ffffe86b66d34a8376710357245718a3ab
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2026-04-09 19:22:00 +0100

    created routes.js file. Exported two named routes. routeHello returns a simple string and routeAPINames makes an API call and returns the data after it is parsed and shaped.

---
+++ routes.js
@@ -0,0 +1,21 @@
+const routeHello = () => 'Hello World - Route!';
+
+const routeAPINames = async () => {
+  const url = 'https://usemodernfullstack.dev/api/v1/users';
+  let data;
+
+  try {
+    const response = await fetch(url);
+    data = await response.json();
+  } catch (err) {
+    return err;
+  }
+
+  const names = data
+    .map(item => `id: ${item.id}, name: ${item.name}`)
+    .join('<br>');
+
+  return names;
+};
+
+export { routeHello, routeAPINames };
